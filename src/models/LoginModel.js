58e57435b08a1e3d43cebdc77e3a284f5b02933e
willsantos96 <willsantos96@gmail.com>
2022-04-11 21:18:28 -0300

    validacao

---
+++ src/models/LoginModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const LoginSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  senha: { type: String, required: true },
+});
+
+const LoginModel = mongoose.model('Login', LoginSchema);
+
+class Login {
+  constructor(body) {
+    this.body = body;
+    this.errors = [];
+    this.user = null;
+  }
+
+  register() {
+    this.valida();
+    if (this.errors.length > 0) return;
+  }
+
+  valida() {
+    this.cleanUp();
+    // Validação
+    // O email precisa ser valido
+    if (!validator.isEmail(this.body.email)) this.errors.push('Email inválido');
+    // A senha precisa ter entre 3 e 50 caracteres
+    if (this.body.password.length < 3 || this.body.password.length > 50) {
+      this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
+    }
+  }
+
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
+        this.body[key] = '';
+      }
+    }
+
+    this.body = {
+      email: this.body.email,
+      password: this.body.password,
+    };
+  }
+}
+
+module.exports = Login;
